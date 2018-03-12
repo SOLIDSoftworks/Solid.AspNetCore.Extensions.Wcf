ecb2c56db104982067b0a271a4539667e715f7f7
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2018-03-12 17:35:41 +0000

    adds new base address factory

--- Solid.AspNetCore.Extensions.Wcf/Factories/BaseAddressFactory.cs
+++ Solid.AspNetCore.Extensions.Wcf/Factories/BaseAddressFactory.cs
@@ -1,4 +1,6 @@
-﻿using Solid.AspNetCore.Extensions.Wcf.Abstractions;
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Solid.AspNetCore.Extensions.Wcf.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,35 @@ namespace Solid.AspNetCore.Extensions.Wcf.Factories
 {
     internal class BaseAddressFactory : IBaseAddressFactory
     {
-        public Uri Create(string path)
+        private IServer _server;
+
+        public BaseAddressFactory(IServer server)
         {
+            _server = server;
+        }
+
+        public IEnumerable<Uri> Create(string path)
+        {
+            return Create(path, false);
+        }
+
+        public IEnumerable<Uri> Create(string path, bool direct)
+        {
+            if (direct)
+                return GetHostAddresses().Select(u => new Uri(u, path));
+
             var host = new Uri($"http://localhost:{GetFreePort()}");
-            return new Uri(host, path);
+            return new[] { new Uri(host, path) };
+        }
+
+        private IEnumerable<Uri> GetHostAddresses()
+        {
+            var addresses = _server.Features[typeof(IServerAddressesFeature)] as IServerAddressesFeature;
+
+            if (addresses == null)
+                return Enumerable.Empty<Uri>();
+
+            return addresses.Addresses.Select(a => new Uri(a));
         }
 
         private int GetFreePort()
